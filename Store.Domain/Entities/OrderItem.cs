f10c6b24379f30ccbede9a8a5ae0483bde4a8abb
Rafael Santos <rafa.05125@gmail.com>
2026-04-06 10:38:15 -0300

    OrderItem validation

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,9 +1,18 @@
+using Flunt.Validations;
+
 namespace Store.Domain.Entities
 {
     public class OrderItem : Entity
     {
         public OrderItem(Product product, int quantity)
         {
+
+            AddNotifications(new Contract()
+                  .Requires()
+                  .IsNotNull(product, "Product", "Produto inválido")
+                  .IsGreaterThan(quantity, 0, "Quantidade", "A quantidade deve ser maior que zero")
+          );
+
             Product = product;
             Price = Product != null ? product.Price : 0;
             Quantity = quantity;
