3a1806fd3ecbfae22afbcedf6da49b772da71920
Rafael Santos <rafa.05125@gmail.com>
2026-05-17 15:14:18 -0300

    Repositories mocks

---
+++ Store.Tests/Repositories/FakeCustomerRepository.cs
@@ -0,0 +1,13 @@
+namespace Store.Tests.Repositories
+{
+    public class FakeCustomerRepository : ICustomerRepository
+    {
+        public Customer Get(string document)
+        {
+            if (document == "12345678911")
+                return new Customer("Bruce Wayne", "batman@balta.io");
+
+            return null;
+        }
+    }
+}
